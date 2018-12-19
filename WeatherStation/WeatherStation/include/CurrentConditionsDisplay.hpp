ba637921ea065acfe6655a8c0a1dd1723ec241db
didix21 <didaccoll_93@hotmail.com>
2018-12-19 23:39:27 +0100

    Ignoring build files and Implemeted CurrentConditionDisplay

---
+++ WeatherStation/WeatherStation/include/CurrentConditionsDisplay.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "Observer.hpp"
+#include "Subject.hpp"
+#include "DisplayElement.hpp"
+
+#include <iostream>
+
+class CurrentConditionDisplay : public Observer, public DisplayElement
+{
+private:
+    float temperature;
+    float humidity;
+    Subject* weatherData;
+
+public:
+    
+
+    CurrentConditionDisplay(Subject* oservable);
+    ~CurrentConditionDisplay();
+    
+    void update(float temperature, float humidity, float pressure) override;
+
+    void display() override;
+
+};
--- WeatherStation/WeatherStation/src/main.cpp
+++ WeatherStation/WeatherStation/src/main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
+#include "WeatherData.hpp"
+#include "CurrentConditionsDisplay.hpp"
 
 int main() {
-    std::cout << "Hello Easy C++ project!" << std::endl;
+    //WeatherData* weatherData = new WeatherData();
+    WeatherData weatherData;
+
+    CurrentConditionDisplay currentCondtionDisplay(&weatherData);
+    //CurrentConditionDisplay* currentCondtionDisplay = new CurrentConditionDisplay(weatherData);
+
+    //weatherData->setMeasurements(19.0, 10.0, 1.0);
+    weatherData.setMeasurements(19.0, 10.0, 1.0);
+
 }
